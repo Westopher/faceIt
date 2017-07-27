c0b00b1f7cc564e17953c3b582795ef75f3aaf56
West Kraemer <westkraemer@wests-mbp.attlocal.net>
2017-07-27 11:29:27 -0400

    address error re adding new fences due to CA restriction

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class FaceView: UIView {
 
+    var scale: CGFloat = 0.9
+    
+    private var skullRadius: CGFloat {
+     return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+    private var skullCenter: CGPoint {
+    return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    private enum Eye {
+        case left
+        case right
+    }
+    
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        
+        
+        
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
 
     override func draw(_ rect: CGRect) {
         
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
         path.lineWidth = 5.0
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
         
     }
 
