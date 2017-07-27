ef54274b7ff21351e8359006d4875848635fbfb4
West Kraemer <westkraemer@wests-mbp.attlocal.net>
2017-07-27 11:58:57 -0400

    remove previous bug fix, as it only diabled message in the command center, it did not address the problem. add eyes and stroke path to get them drawn.

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class FaceView: UIView {
 
-    var scale: CGFloat = 0.9
+    var scale: CGFloat = 0.8
     
     private var skullRadius: CGFloat {
      return min(bounds.size.width, bounds.size.height) / 2
@@ -27,8 +27,19 @@ class FaceView: UIView {
     
     private func pathForEye(_ eye: Eye) -> UIBezierPath {
         
+        func centerofEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratios.skullRaduisToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        let eyeRadius = skullRadius / Ratios.skullRaduisToEyeRadius
+        let eyeCenter = centerofEye(eye)
         
-        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        path.lineWidth = 5.0
+        return path
     }
     
     private func pathForSkull() -> UIBezierPath {
@@ -38,11 +49,21 @@ class FaceView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
         path.lineWidth = 5.0
         UIColor.blue.set()
         pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
+        
+    }
+    
+    private struct Ratios {
+        static let skullRaduisToEyeOffset: CGFloat = 3
+        static let skullRaduisToEyeRadius: CGFloat = 10
+        static let skullRaduisToMouthWidth: CGFloat = 1
+        static let skullRaduisToMouthHeight: CGFloat = 3
+        static let skullRaduisToMouthOffset: CGFloat = 3
         
     }
 
