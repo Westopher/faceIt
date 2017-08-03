387301e780de2683847c909148717da6b4887bd8
West Kraemer <kraemerwest@gmail.com>
2017-08-03 11:17:58 -0400

    add additional view controller for emotions with buttons that are stacked and are reset to suggested constraints

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by West Kraemer on 8/3/17.
+//  Copyright © 2017 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
